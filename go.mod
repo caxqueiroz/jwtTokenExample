6a16526db0af3bfec81464d031587685f08ecd83
Carlos Queiroz <caxqueiroz@gmail.com>
2021-01-30 17:07:44 +0800

    added redis support for refresh jwt token.

--- go.mod
+++ go.mod
@@ -5,4 +5,8 @@ go 1.15
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.6.3
+	github.com/go-redis/redis/v7 v7.4.0
+	github.com/myesui/uuid v1.0.0 // indirect
+	github.com/twinj/uuid v1.0.0
+	gopkg.in/stretchr/testify.v1 v1.2.2 // indirect
 )
--- jwtex/main.go
+++ jwtex/main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v7"
+	"github.com/twinj/uuid"
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
-var (
-	router = gin.Default()
-)
+var router = gin.Default()
+var client *redis.Client
+
+func init() {
+	//Initializing redis
+	dsn := os.Getenv("REDIS_DSN")
+	if len(dsn) == 0 {
+		dsn = "localhost:6379"
+	}
+	client = redis.NewClient(&redis.Options{
+		Addr: dsn, //redis port
+	})
+	_, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
+}
 
 type User struct {
 	ID       uint64 `json:"id"`
@@ -26,21 +45,99 @@ var user = User{
 	Password: "123",
 }
 
-func CreateToken(userid uint64) (string, error) {
+type AccessDetails struct {
+	AccessUuid string
+	UserId     uint64
+}
+
+type TokenDetails struct {
+	AccessToken  string
+	RefreshToken string
+	AccessUuid   string
+	RefreshUuid  string
+	AtExpires    int64
+	RtExpires    int64
+}
+
+func CreateAuth(userid uint64, td *TokenDetails) error {
+	at := time.Unix(td.AtExpires, 0) //converting Unix to UTC(to Time object)
+	rt := time.Unix(td.RtExpires, 0)
+	now := time.Now()
+
+	errAccess := client.Set(td.AccessUuid, strconv.Itoa(int(userid)), at.Sub(now)).Err()
+	if errAccess != nil {
+		return errAccess
+	}
+	errRefresh := client.Set(td.RefreshUuid, strconv.Itoa(int(userid)), rt.Sub(now)).Err()
+	if errRefresh != nil {
+		return errRefresh
+	}
+	return nil
+}
+
+func ExtractToken(r *http.Request) string {
+	bearToken := r.Header.Get("Authorization")
+	strArr := strings.Split(bearToken, " ")
+	if len(strArr) == 2 {
+		return strArr[1]
+	}
+	return ""
+}
+
+// Parse, validate, and return a token.
+// keyFunc will receive the parsed token and should return the key for validating.
+func VerifyToken(r *http.Request) (*jwt.Token, error) {
+	tokenString := ExtractToken(r)
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(os.Getenv("ACCESS_SECRET")), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token, nil
+}
+
+func CreateToken(userid uint64) (*TokenDetails, error) {
+
+	td := &TokenDetails{}
+	td.AtExpires = time.Now().Add(time.Minute * 15).Unix()
+	td.AccessUuid = uuid.NewV4().String()
+	td.RtExpires = time.Now().Add(time.Hour * 24 * 7).Unix()
+	td.RefreshUuid = td.AccessUuid + "++" + strconv.Itoa(int(userid))
+
 	var err error
 	// create access token
 	os.Setenv("ACCESS_SECRET", "fdnfsxdnfkab")
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["user_id"] = userid
-	claims["exp"] = time.Now().Add(time.Minute * 15).Unix()
+	claims["refresh_uuid"] = td.RefreshUuid
+	claims["exp"] = td.AtExpires
+
 	at := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	token, err := at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
+
+	td.AccessToken, err = at.SignedString([]byte(os.Getenv("ACCESS_SECRET")))
 
 	if err != nil {
-		return "", err
+		return nil, err
 	}
-	return token, err
+
+	//Creating Refresh Token
+	os.Setenv("REFRESH_SECRET", "mcmvmkmsdnfsdmfdsjf") //this should be in an env file
+	rtClaims := jwt.MapClaims{}
+	rtClaims["refresh_uuid"] = td.RefreshUuid
+	rtClaims["user_id"] = userid
+	rtClaims["exp"] = td.RtExpires
+	rt := jwt.NewWithClaims(jwt.SigningMethodHS256, rtClaims)
+	td.RefreshToken, err = rt.SignedString([]byte(os.Getenv("REFRESH_SECRET")))
+	if err != nil {
+		return nil, err
+	}
+
+	return td, err
 }
 
 func Login(c *gin.Context) {
@@ -55,14 +152,22 @@ func Login(c *gin.Context) {
 		return
 	}
 	// if reach this point, all checked out.
-	token, err := CreateToken(user.ID)
+	td, err := CreateToken(user.ID)
 
 	if err != nil {
 		c.JSON(http.StatusUnprocessableEntity, err.Error())
 		return
 	}
+	saveErr := CreateAuth(user.ID, td)
+	if saveErr != nil {
+		c.JSON(http.StatusUnprocessableEntity, saveErr.Error())
+	}
+	tokens := map[string]string{
+		"access_token":  td.AccessToken,
+		"refresh_token": td.RefreshToken,
+	}
 
-	c.JSON(http.StatusOK, token)
+	c.JSON(http.StatusOK, tokens)
 
 }
 
